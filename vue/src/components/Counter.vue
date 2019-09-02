ebe7f69d833672f11617b29dc813f9f170eb8394
beichengguiyan <chihaotian@zybank.com.cn>
2019-09-02 17:32:51 +0800

    test

---
+++ vue/src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="counter">
+    <button class = "click me" @click="add()">+</button>{{count}}
+    <button class = "click me" @click="sub()">-</button>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'counter',
+  data () {
+    return {
+      count : 0,
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods : {
+    add : function() {
+        this.count++;
+    },
+    sub: function() {
+        this.count--;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button{
+margin-right:20px;
+margin-left:20px;
+
+}
+</style>
---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>counter</h2>
+    <input id="text" v-model.number="num" />
+    <div v-for="item in num" :key="item">
+      {{item}}
+      <p>counter</p>  
+      <Counter></Counter>
+    </div>
+  </div>
+</template>
+
+<script>
+import Counter from "./Counter";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      num: 0,
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  components: {
+    Counter
+  },
+  Methods: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
